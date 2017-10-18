dbbd9cb13204acc50fab2ce0078b3d2fc1152720
Michael Fiano <michael.fiano@gmail.com>
2017-10-18 01:34:09 -0400

    Minor changes

--- src/common.lisp
+++ src/common.lisp
@@ -4,13 +4,13 @@
   (chipz:decompress nil compression-scheme octet-vector
                     :buffer-size (* (length octet-vector) 2)))
 
-(defun %string-length (bytes null-terminated)
-  (let* ((octet-vector (fast-io:input-buffer-vector (buffer-bytes)))
-         (max-length (or bytes (length octet-vector)))
+(defun %string-length (bytes null-terminated-p)
+  (let* ((sequence (fast-io:input-buffer-vector (buffer-bytes)))
+         (max-length (or bytes (length sequence)))
          (start (fast-io:buffer-position (buffer-bytes)))
-         (end (min (length octet-vector) (+ start max-length)))
-         (index (if null-terminated
-                    (position 0 octet-vector :start start :end end)
+         (end (min (length sequence) (+ start max-length)))
+         (index (if null-terminated-p
+                    (position 0 sequence :start start :end end)
                     end)))
     (- index start)))
 
