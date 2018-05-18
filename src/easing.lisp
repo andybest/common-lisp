a9a131627d0575ec4379c2848df7b0207602a939
Michael Fiano <mail@michaelfiano.com>
2018-05-17 20:46:01 -0400

    Expand expt to repeated multiplication due to undefined behavior

--- src/easing.lisp
+++ src/easing.lisp
@@ -1,7 +1,7 @@
 (in-package :umbra.easing)
 
 ;;;; Robert Penner's Easing functions
-;;;; Specify the rate of change a parameter changes over time.
+;;;; Specify the rate of change for a parameter over time.
 ;;;; http://www.robertpenner.com/easing
 
 (defun-gpu linear ((x :float))
@@ -14,7 +14,7 @@
   (1+ (sin (* (1- x) +half-pi+))))
 
 (defun-gpu sine-in-out ((x :float))
-  (* 0.5 (1- (cos (* x +pi+)))))
+  (* 0.5 (- 1 (cos (* x +pi+)))))
 
 (defun-gpu quadratic-out ((x :float))
   (- (* x (- x 2))))
@@ -28,42 +28,49 @@
       (1- (+ (* x x -2) (* x 4)))))
 
 (defun-gpu cubic-out ((x :float))
-  (1+ (expt (1- x) 3)))
+  (let ((f (1- x)))
+    (1+ (* f f f))))
 
 (defun-gpu cubic-in ((x :float))
-  (expt x 3))
+  (* x x x))
 
 (defun-gpu cubic-in-out ((x :float))
   (if (< x 0.5)
-      (* (expt x 3) 4)
-      (1+ (* 0.5 (expt (- (* x 2) 2) 3)))))
+      (* x x x 4)
+      (let ((f (- (* x 2) 2)))
+        (1+ (* 0.5 f f f))))))
 
 (defun-gpu quartic-out ((x :float))
-  (1+ (* (expt (1- x) 3) (- 1 x))))
+  (let ((f (1- x)))
+    (1+ (* f f f (- 1 x)))))
 
 (defun-gpu quartic-in ((x :float))
-  (expt x 4))
+  (* x x x x))
 
 (defun-gpu quartic-in-out ((x :float))
   (if (< x 0.5)
-      (* (expt x 4) 8)
-      (1+ (* -8 (expt (1- x) 4)))))
+      (* x x x x 8)
+      (let ((f (1- x)))
+        (1+ (* -8 f f f f)))))
 
 (defun-gpu quintic-out ((x :float))
-  (1+ (expt (1- x) 5)))
+  (let ((f (- x 1)))
+    (1+ (* f f f f f))))
 
 (defun-gpu quintic-in ((x :float))
-  (expt x 5))
+  (* x x x x x))
 
 (defun-gpu quintic-in-out ((x :float))
   (if (< x 0.5)
-      (* (expt x 5) 16)
-      (1+ (* 0.5 (expt (- (* x 2) 2) 5)))))
+      (* x x x x x 16)
+      (let ((f (- (* x 2) 2)))
+        (1+ (* 0.5 f f f f f)))))
 
 (defun-gpu exponential-out ((x :float))
   (if (= x 1)
       x
-      (- 1 (expt 2 (* x -10)))))
+      (let ((f (* x -10)))
+        (- 1 (expt 2 (* x -10))))))
 
 (defun-gpu exponential-in ((x :float))
   (if (zerop x)
@@ -80,7 +87,7 @@
      (1+ (* -0.5 (expt 2 (+ (* x -20) 10)))))))
 
 (defun-gpu circular-out ((x :float))
-  (sqrt (* (- 2 x) x)))
+  (sqrt (* (- 2 x) x))
 
 (defun-gpu circular-in ((x :float))
   (- 1 (sqrt (- 1 (* x x)))))
@@ -91,18 +98,18 @@
       (* 0.5 (1+ (sqrt (- (* (- (* 2 x) 3) (1- (* 2 x)))))))))
 
 (defun-gpu back-out ((x :float))
-  (let ((v (- 1 x)))
-    (- 1 (- (expt v 3)) (* v (sin (* v +pi+))))))
+  (let ((f (- 1 x)))
+    (- 1 (- (* f f f) (* f (sin (* f +pi+)))))))
 
 (defun-gpu back-in ((x :float))
-  (- (expt x 3) (* x (sin (* x +pi+)))))
+  (- (* x x x) (* x (sin (* x +pi+)))))
 
 (defun-gpu back-in-out ((x :float))
   (if (< x 0.5)
-      (let ((v (* x 2)))
-        (* 0.5 (- (expt v 3) (* v (sin (* v +pi+))))))
-      (let ((v (- 2 (* x 2))))
-        (+ (* 0.5 (- 1 (- (expt v 3) (* v (sin (* v +pi+)))))) 0.5))))
+      (let ((f (* x 2)))
+        (* 0.5 (- (* f f f) (* f (sin (* f +pi+))))))
+      (let ((f (- 2 (* x 2))))
+        (+ (* 0.5 (- 1 (- (* f f f) (* f (sin (* f +pi+)))))) 0.5))))
 
 (defun-gpu elastic-out ((x :float))
   (1+ (* (sin (* -13 +half-pi+ (1+ x))) (expt 2 (* -10 x)))))
@@ -132,6 +139,7 @@
   (- 1 (bounce-out (- 1 x))))
 
 (defun-gpu bounce-in-out ((x :float))
-  (if (< x 0.5)
-      (* 0.5 (bounce-in (* x 2)))
-      (+ 0.5 (* 0.5 (bounce-out (1- (* x 2)))))))
+  (let ((f (* x 2)))
+    (if (< x 0.5)
+        (* 0.5 (bounce-in f))
+        (+ 0.5 (* 0.5 (bounce-out (1- f)))))))
