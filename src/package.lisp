3ec7e7bb901fb2c2cfaf9d06c4e745254dc69108
Michael Fiano <mail@michaelfiano.com>
2018-05-25 02:56:04 -0400

    Added more hashing functions.

--- src/package.lisp
+++ src/package.lisp
@@ -118,6 +118,13 @@
   (:use #:umbra.common)
   (:export #:blum-blum-shub
            #:blum-blum-shub/hq
-           #:permutation-polynomial
+           #:sgpp
+           #:sgpp/2-per-corner
+           #:sgpp/3-per-corner
            #:fast32
-           #:fast32-2))
+           #:fast32/2-per-corner
+           #:fast32/3-per-corner
+           #:fast32/4-per-corner
+           #:fast32/cell
+           #:fast32-2
+           #:fast32-2/4-per-corner))
