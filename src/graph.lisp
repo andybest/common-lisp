8da68fa5dd07ab899fd5eb2d50b7ebf0c4463072
Michael Fiano <mail@michaelfiano.com>
2018-08-30 18:38:37 -0400

    Switched graph function to use dotimes

--- src/graph.lisp
+++ src/graph.lisp
@@ -2,8 +2,8 @@
 
 ;;;; Graphing
 ;;;; Credits:
-;;;; Mikael Hvidtfeldt Christensen http://blog.hvidtfeldts.net/index.php/2011/07/plotting-high-frequency-functions-using-a-gpu/
-;;;; Chris Bagley https://github.com/cbaggers/nineveh/blob/master/graphing/graph.lisp
+;;;; Mikael Hvidtfeldt Christensen
+;;;; http://blog.hvidtfeldts.net/index.php/2011/07/plotting-high-frequency-functions-using-a-gpu/
 
 (define-gpu-function graph ((func (function (:float) :float))
                             (uv :vec2)
@@ -17,11 +17,11 @@
          (step (/ max-dist samples))
          (count 0.0)
          (my-samples 0))
-    (for (i 0) (< i samples) (++ i)
-         (let ((f (funcall func (+ (.x uv) (* i (.x step))))))
-           (for (j 0) (< j samples) (++ j)
-                (let ((diff (- f (+ (.y uv) (* j (.y step))))))
-                  (incf my-samples)
-                  (incf count (1- (* (step 0 diff) 2)))))))
+    (dotimes (i samples)
+      (let ((f (funcall func (+ (.x uv) (* i (.x step))))))
+        (dotimes (j samples)
+          (let ((diff (- f (+ (.y uv) (* j (.y step))))))
+            (incf my-samples)
+            (incf count (1- (* (step 0 diff) 2)))))))
     (* (- 1 (/ (abs count) my-samples))
        (vec4 (.xyz line-style) 1))))
