d8a4461de6720fb3c308557ee63efb1e5d833d66
Bart Botta <00003b@gmail.com>
2019-06-09 09:48:17 -0500

    use binpack rect API in unpacker

--- src/unpacker.lisp
+++ src/unpacker.lisp
@@ -25,17 +25,16 @@
                     :y (round (* atlas-height y))
                     :w (round (* atlas-width w))
                     :h (round (* atlas-height h)))))
-        (with-slots (%y %h) rect
-          (when flip-y
-            (setf %y (- atlas-height %y %h))))
+        (when flip-y
+          (setf (y rect) (- atlas-height (y rect) (h rect) )))
         rect))))
 
 (defun write-sprite (atlas rect out-file)
-  (with-slots (%x %y %w %h) rect
-    (let ((sprite (opticl:make-8-bit-rgba-image %h %w)))
+  (binpack:with-rect (nil x y w h) rect
+    (let ((sprite (opticl:make-8-bit-rgba-image h w)))
       (opticl:do-pixels (i j) sprite
         (setf (opticl:pixel sprite i j)
-              (opticl:pixel atlas (+ i %y) (+ j %x))))
+              (opticl:pixel atlas (+ i y) (+ j x))))
       (opticl:write-image-file out-file sprite))))
 
 (defun unpack-sprite (atlas data denormalize flip-y out-path)
