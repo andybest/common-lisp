df5a838a2a6f872afa3a225a0d62a4946861706d
Michael Fiano <mail@michaelfiano.com>
2020-01-11 18:33:26 -0500

    fix unproject

--- src/general.lisp
+++ src/general.lisp
@@ -8,7 +8,8 @@
       (return-from unproject! out))
     (m4:with-components ((m inverse-pm))
       (v4:with-components ((v viewport))
-        (v3:with-components ((p point))
+        (v3:with-components ((p point)
+                             (out out))
           (v4:with-elements ((o 0f0 0f0 0f0 0f0)
                              (i (1- (/ (* (- px vx) 2) vz))
                                 (1- (/ (* (- py vy) 2) vw))
@@ -20,7 +21,7 @@
                        ix iy iz iw)
             (when (zerop ow)
               (return-from unproject! out))
-            (v3:scale! out out (/ ow)))))))
+            (v3::%scale outx outy outz ox oy oz (/ ow)))))))
   (values out t))
 
 (define-op unproject ((point v3:vec) (model m4:mat) (projection m4:mat)
