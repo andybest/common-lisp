8b7caef4dc2dcfac43ed874e8f8a6c6cdb4727e1
Michael Fiano <mail@michaelfiano.com>
2020-01-19 03:35:18 -0500

    minor change

--- src/general.lisp
+++ src/general.lisp
@@ -34,11 +34,11 @@
     (:out v3:vec)
   (v3:+ point (v3:scale direction distance)))
 
-(define-op line-direction ((point1 v3:vec) (point2 v3:vec)) (:out v3:vec)
-  (v3:normalize (v3:- point2 point1)))
-
 (define-op line-segment-midpoint ((point1 v3:vec) (point2 v3:vec)) (:out v3:vec)
   (v3:lerp point1 point2 0.5))
+
+(define-op line-direction ((point1 v3:vec) (point2 v3:vec)) (:out v3:vec)
+  (v3:normalize (v3:- point2 point1)))
 
 (define-op line-plane-intersect ((line-point1 v3:vec) (line-point2 v3:vec)
                                  (plane-point v3:vec) (plane-normal v3:vec))
